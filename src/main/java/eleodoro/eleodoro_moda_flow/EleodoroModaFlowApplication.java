723461a56c8e7ec4b217ef1d741d2c5564c47991
joao091 <joao.pedrosilvade.sa@escola.pr.gov.br>
2024-11-19 12:05:50 -0300

    commit

---
+++ src/main/java/eleodoro/eleodoro_moda_flow/EleodoroModaFlowApplication.java
@@ -0,0 +1,13 @@
+package eleodoro.eleodoro_moda_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EleodoroModaFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EleodoroModaFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eleodoro/eleodoro_moda_flow/EleodoroModaFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package eleodoro.eleodoro_moda_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EleodoroModaFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
