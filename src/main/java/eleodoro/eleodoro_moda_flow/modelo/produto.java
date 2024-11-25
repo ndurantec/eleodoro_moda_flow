2d3152ccb4a0f5185359888a861c31967991f325
Zecapaugordinho69 <joas.martins@escola.pr.gov.br>
2024-11-25 11:51:04 -0300

    fiz a classe produto

---
+++ src/main/java/eleodoro/eleodoro_moda_flow/modelo/produto.java
@@ -0,0 +1,113 @@
+package eleodoro.eleodoro_moda_flow.modelo;
+
+public class produto {
+    private String nome;
+    private String modelo;
+    private String cor;
+    private int tamanho;
+    private Pedido pedido;
+
+    public produto() {
+    }
+
+    public produto(String nome, String modelo, String cor, int tamanho, Pedido pedido) {
+        this.nome = nome;
+        this.modelo = modelo;
+        this.cor = cor;
+        this.tamanho = tamanho;
+        this.pedido = pedido;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(int tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    @Override
+    public String toString() {
+        return "produto [nome=" + nome + ", modelo=" + modelo + ", cor=" + cor + ", tamanho=" + tamanho + ", pedido="
+                + pedido + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((modelo == null) ? 0 : modelo.hashCode());
+        result = prime * result + ((cor == null) ? 0 : cor.hashCode());
+        result = prime * result + tamanho;
+        result = prime * result + ((pedido == null) ? 0 : pedido.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        produto other = (produto) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (modelo == null) {
+            if (other.modelo != null)
+                return false;
+        } else if (!modelo.equals(other.modelo))
+            return false;
+        if (cor == null) {
+            if (other.cor != null)
+                return false;
+        } else if (!cor.equals(other.cor))
+            return false;
+        if (tamanho != other.tamanho)
+            return false;
+        if (pedido == null) {
+            if (other.pedido != null)
+                return false;
+        } else if (!pedido.equals(other.pedido))
+            return false;
+        return true;
+    }    
+}
+
