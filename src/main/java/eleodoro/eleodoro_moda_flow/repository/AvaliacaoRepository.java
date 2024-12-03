a5d4e9fa63c1bf7fcb838524a2c28f4429ee1f31
Capuzitos6 <c.monteiro@escola.pr.gov.br>
2024-12-03 12:12:09 -0300

    tem erros mas vai

--- src/main/java/eleodoro/eleodoro_moda_flow/repository/AvaliacaoRepository.java
+++ src/main/java/eleodoro/eleodoro_moda_flow/repository/AvaliacaoRepository.java
@@ -4,18 +4,7 @@ import eleodoro.eleodoro_moda_flow.modelo.Avaliacao;
 
 public class AvaliacaoRepository {
 
-    public static void save(Avaliacao novoAvaliacao) {
-    }
-
-    public Object findById(Long id) {
-        return null;
-    }
-
-    public boolean existsById(Long id) {
-        return false;
-    }
-
-    public void deleteById(Long id) {
+    public void save(Avaliacao novoAvaliacao) {
     }
     
 }
