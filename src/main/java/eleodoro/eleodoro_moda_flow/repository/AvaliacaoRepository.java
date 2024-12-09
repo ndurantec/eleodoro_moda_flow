26ddc18ee82679c0b9149a55a6d5468089fbb073
Capuzitos6 <c.monteiro@escola.pr.gov.br>
2024-12-09 11:48:49 -0300

    corrigido erros nas classes

--- src/main/java/eleodoro/eleodoro_moda_flow/repository/AvaliacaoRepository.java
+++ src/main/java/eleodoro/eleodoro_moda_flow/repository/AvaliacaoRepository.java
@@ -1,24 +1,9 @@
 package eleodoro.eleodoro_moda_flow.repository;
 
-import java.util.List;
-import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import eleodoro.eleodoro_moda_flow.modelo.Avaliacao;
 
-public class AvaliacaoRepository {
-
-    public void save(Avaliacao novoAvaliacao) {
-    }
-
-    public Optional<Avaliacao> findById(Long id) {
-        return null;
-    }
-
-    public List<Avaliacao> findAll() {
-        return null;
-    }
-
-    public void delete(Avaliacao avaliacao) {
-    }
+public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Long>{
     
 }
--- src/main/java/eleodoro/eleodoro_moda_flow/repository/EstampaRepository.java
+++ src/main/java/eleodoro/eleodoro_moda_flow/repository/EstampaRepository.java
@@ -1,30 +1,8 @@
 package eleodoro.eleodoro_moda_flow.repository;
 
-import java.util.Optional;
-
-import eleodoro.eleodoro_moda_flow.modelo.Estampa;
-
 public class EstampaRepository {
 
-    public void save(Estampa novoEstampa) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'save'");
-    }
-
-    public Optional<Estampa> findById(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findById'");
-    }
-
-    public boolean existsById(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'existsById'");
-    }
-
-    public void deleteById(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
-    }
+   
 
     
     
