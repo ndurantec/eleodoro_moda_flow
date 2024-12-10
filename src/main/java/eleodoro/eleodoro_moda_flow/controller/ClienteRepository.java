8d63a9ccbb2ea4c15ee647a4be4d1955ea782e7b
Zecapaugordinho69 <joas.martins@escola.pr.gov.br>
2024-12-10 12:10:09 -0300

    controller clintee correção

---
+++ src/main/java/eleodoro/eleodoro_moda_flow/controller/ClienteRepository.java
@@ -0,0 +1,31 @@
+package eleodoro.eleodoro_moda_flow.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import eleodoro.eleodoro_moda_flow.modelo.Cliente;
+
+public enum ClienteRepository {
+    ;
+
+    void save(Cliente novoCliente) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    void delete(Cliente cliente) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    Optional<Cliente> findById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    List<Cliente> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+}
